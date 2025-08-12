69a225e147b1d7bd24332e6b3bd14e7e3353de6a
alexanderbenerink <alexander.benerink@outlook.com>
2025-08-12 12:00:46 +0200

    Added API endpoints for board crud

--- app/Http/Controllers/API/BoardController.php
+++ app/Http/Controllers/API/BoardController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Board;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BoardController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $boards = $request->user()->boards()->with('tasks')->latest()->get();
+
+        return response()->json(['boards' => $boards]);
     }
 
     /**
@@ -20,30 +24,78 @@ class BoardController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(['name' => 'required|string|max:255']);
+
+        $board = $request->user()->boards()->create(['name' => $request->name]);
+
+        $board->load('tasks');
+
+        return response()->json([
+            'board' => $board,
+            'message' => 'Board created'
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, Board $board)
     {
-        //
+        if ($board->user_id !== $request->user()->id) {
+            return response()->json(['message' => 'Board not found'], 404);
+        }
+
+        $board->load(['tasks' => function($query) {
+            $query->orderBy('position');
+        }]);
+
+        return response()->json([
+            'board' => $board
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Board $board)
     {
-        //
+        if ($board->user_id !== $request->user()->id) {
+            return response()->json([
+                'message' => 'Board not found'
+            ], 404);
+        }
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $board->update([
+            'name' => $request->name,
+        ]);
+
+        $board->load('tasks');
+
+        return response()->json([
+            'board' => $board,
+            'message' => 'Board updated'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, Board $board)
     {
-        //
+        if ($board->user_id !== $request->user()->id) {
+            return response()->json([
+                'message' => 'Board not found'
+            ], 404);
+        }
+
+        $board->delete();
+
+        return response()->json([
+            'message' => 'Board deleted'
+        ]);
     }
 }
